04aa6272e9a004f70f5c35c0ab9b24cd796c4efc
Женя <laeda@list.ru>
2016-10-26 22:11:07 +0300

    Second commit

---
+++ shipmentsWeb/app/app.js
@@ -0,0 +1,43 @@
+﻿angular.module('shipments', [
+    'auth0',
+    'ngRoute',
+    'shipments.home',
+    'shipments.login',
+    'angular-storage',
+    'angular-jwt'
+])
+
+.config(function ($routeProvider, authProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: 'app/components/home/home.html',
+            requiresLogin: true
+        })
+        .when('/login', {
+            controller: 'LoginCtrl',
+            templateUrl: 'app/components/login/login.html'
+        });
+
+    authProvider.init({
+        domain: AUTH0_DOMAIN,
+        clientID: AUTH0_CLIENT_ID,
+        loginUrl: '/login'
+    });
+})
+
+.run(function ($rootScope, auth, store, jwtHelper, $location) {
+    $rootScope.$on('$locationChangeStart', function () {
+       if (!auth.isAuthenticated) {
+           var token = store.get('token');
+           if (token) {
+               if (!jwtHelper.isTokenExpired(token)) {
+                   auth.authenticate(store.get('profile'), token);
+               } else {
+                   $location.path('/login');
+               }
+           }
+       }
+    });
+    auth.hookEvents();
+});
---
+++ shipmentsWeb/app/config.js
@@ -0,0 +1,3 @@
+﻿var AUTH0_DOMAIN = 'strelokdp.eu.auth0.com';
+var AUTH0_CLIENT_ID = 'sqBrHG3OsRpKAkgZXo5UDfDr89Y7IUw4';
+var SERVICE_BASE = 'http://localhost:53329';
