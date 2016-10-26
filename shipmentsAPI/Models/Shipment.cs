bca806f707e278c34f77c852c4ab59059f807469
Женя <laeda@list.ru>
2016-10-26 22:09:43 +0300

    First commit

---
+++ shipmentsAPI/Models/Shipment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shipmentsAPI.Models
+{
+    public class Shipment
+    {
+        public int Id { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+
+        public static List<Shipment> Create()
+        {
+            return new List<Shipment>
+            {
+                new Shipment
+                {
+                    Id = 123,
+                    Destination = "Norrköping",
+                    Origin = "Linköping"
+                },
+                new Shipment
+                {
+                    Id = 343,
+                    Destination = "Stockholm",
+                    Origin = "Göteborg"
+                }
+            };
+        }
+    }
+}
---
+++ shipmentsAPI/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using shipmentsAPI.App_Start;
+using System.Web.Http;
+using System.Configuration;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler.Encoder;
+using Microsoft.Owin.Security.Jwt;
+
+[assembly: OwinStartup(typeof(shipmentsAPI.Startup))]
+namespace shipmentsAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            ConfigureAuthZero(app);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        private void ConfigureAuthZero(IAppBuilder app)
+        {
+            var issuer = "https://" + ConfigurationManager.AppSettings["auth0::Domain"] + "/";
+            var audience = ConfigurationManager.AppSettings["auth0::ClientId"];
+            var secret = TextEncodings.Base64.Encode(TextEncodings.Base64Url.Decode(ConfigurationManager.AppSettings["auth0::ClientSecret"]));
+
+            app.UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions
+            {
+                AuthenticationMode = AuthenticationMode.Active,
+                AllowedAudiences = new[] { audience },
+                IssuerSecurityTokenProviders = new[]
+                {
+                    new SymmetricKeyIssuerSecurityTokenProvider(issuer, secret)
+                }
+            });
+        }
+    }
+}
